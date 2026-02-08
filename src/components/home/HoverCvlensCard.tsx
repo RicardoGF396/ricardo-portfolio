67744b4a94f80a05cea189873159fc4df046c5fe
Ricardo <ricardogf396@gmail.com>
2026-02-08 17:54:56 -0600

    fix: papers name

--- src/components/home/HoverCvlensCard.tsx
+++ src/components/home/HoverCvlensCard.tsx
@@ -19,17 +19,17 @@ export function HoverCvlensCard() {
           />
           <img
             className="animate-all absolute bottom-0 left-3 z-10 w-[100px] duration-300 group-hover:-translate-x-8 group-hover:-translate-y-3 group-hover:-rotate-30"
-            src="/paper.png"
+            src="/Paper.png"
             alt="paper-right"
           />
           <img
             className="animate-all absolute bottom-0 left-3 z-10 w-[100px] duration-300 group-hover:translate-x-6 group-hover:-translate-y-1 group-hover:rotate-30"
-            src="/paper.png"
+            src="/Paper.png"
             alt="paper-right"
           />
           <img
             className="animate-all absolute bottom-0 left-3 z-10 w-[100px] duration-300 group-hover:-translate-y-2"
-            src="/paper.png"
+            src="/Paper.png"
             alt="paper-center"
           />
 
